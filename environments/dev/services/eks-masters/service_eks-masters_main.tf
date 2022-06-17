05b14e5c696cb83a1bafdc584a86c613f800b204
thevictorgreen <victor.green@techsur.solutions>
2022-06-17 12:35:36 -0400

    ds up 3

--- environments/dev/services/eks-masters/service_eks-masters_main.tf
+++ environments/dev/services/eks-masters/service_eks-masters_main.tf
@@ -1,12 +1,12 @@
 data "aws_caller_identity" "current" {}
-
+/*
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
-}
+}*/
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
