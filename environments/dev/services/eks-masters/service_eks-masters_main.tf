d6ea1a3c3ea2281293ea978f84f6685a9a87fdcb
thevictorgreen <victor.green@techsur.solutions>
2022-06-20 16:37:08 -0400

    ds up 6

--- environments/dev/services/eks-masters/service_eks-masters_main.tf
+++ environments/dev/services/eks-masters/service_eks-masters_main.tf
@@ -44,25 +44,18 @@ data "terraform_remote_state" "vpc" {
 
 
 module "eks" {
-  source       = "../../modules/eks-masters"
-  cluster_name = local.cluster_name
-  //cluster_role_arn        = data.terraform_remote_state.dataplatform-iam.outputs.cluster_role_arn
+  source           = "../../modules/eks-masters"
+  cluster_name     = local.cluster_name
   cluster_role_arn = module.eks_master_iam.cluster_role_arn
   cluster_version  = "1.21"
   private_subnets  = data.terraform_remote_state.vpc.outputs.private_subnets
-  //dd_api_key              = data.aws_secretsmanager_secret_version.dd_api_key.secret_string
-  //dd_api_key_arn          = data.aws_secretsmanager_secret_version.dd_api_key.arn
-  //dd_app_key              = data.aws_secretsmanager_secret_version.dd_app_key.secret_string
-  //external_secrets_region = var.region.Ohio
-  environment = var.environment
-  //public_ingress_cert_arn = data.aws_acm_certificate.k8s_ingress_certificate.arn
+  environment      = var.environment
+  vpc_id           = data.terraform_remote_state.vpc.outputs.vpc_id
 
   tags = {
     Environment = var.environment
     Owner       = "data-platform"
   }
-
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
   node_k8s_labels = {
     Environment = var.environment
@@ -71,5 +64,11 @@ module "eks" {
   node_additional_tags = {
 
   }
+
+  //dd_api_key              = data.aws_secretsmanager_secret_version.dd_api_key.secret_string
+  //dd_api_key_arn          = data.aws_secretsmanager_secret_version.dd_api_key.arn
+  //dd_app_key              = data.aws_secretsmanager_secret_version.dd_app_key.secret_string
+  //external_secrets_region = var.region.Ohio
+  //public_ingress_cert_arn = data.aws_acm_certificate.k8s_ingress_certificate.arn
 
 }
