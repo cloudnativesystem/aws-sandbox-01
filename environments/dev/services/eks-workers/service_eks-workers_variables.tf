6529bd17cdcc4ffa153ab8255b28352c01114cb7
thevictorgreen <victor.green@techsur.solutions>
2022-06-21 08:06:42 -0400

    ds up 19

--- environments/dev/services/eks-workers/service_eks-workers_variables.tf
+++ environments/dev/services/eks-workers/service_eks-workers_variables.tf
@@ -14,26 +14,4 @@ variable "primary_node_group_settings" {
     taint_effect   = string
     tags           = map(string)
   })
-
-  default = {
-    name           = "primary-ng"
-    desired_size   = 1
-    max_size       = 3
-    min_size       = 1
-    instance_types = ["m5.large"]
-    capacity_type  = "ON_DEMAND"
-    disk_size      = 50
-    labels = {
-      "team" = "general-ng"
-    }
-    has_taint    = false
-    taint_key    = "NA"
-    taint_value  = "NA"
-    taint_effect = "NA"
-    tags = {
-      "Environment" = "dev"
-      "Owner"       = "swagwatch"
-      "Cost-Center" = "general"
-    }
-  }
 }
