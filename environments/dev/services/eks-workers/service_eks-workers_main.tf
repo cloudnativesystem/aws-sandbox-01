1e512276d632e3dcb04303b58e9093bcaa52f970
thevictorgreen <victor.green@techsur.solutions>
2022-06-21 07:01:46 -0400

    ds up 15

--- environments/dev/services/eks-workers/service_eks-workers_main.tf
+++ environments/dev/services/eks-workers/service_eks-workers_main.tf
@@ -2,7 +2,7 @@ module "primary" {
   source = "../../modules/eks-workers"
 
   cluster_name        = data.terraform_remote_state.remote_state.outputs.cluster_id
-  node_role_arn       = module.eks_master_iam.worker_node_arn
+  node_role_arn       = module.eks_worker_iam.worker_node_arn
   subnet_ids          = data.terraform_remote_state.remote_state.outputs.private_subnets
   node_group_settings = var.primary_node_group_settings
 }
